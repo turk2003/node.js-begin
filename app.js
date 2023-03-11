59a3506cd86edd91b520d7bf43838d65933bf652
turk2003 <76483183+turk2003@users.noreply.github.com>
2023-03-11 14:16:46 +0700

    template

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 const debug  = require('debug','app')
 const morgan  = require('morgan')
+const path = require('path');
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,'/public/')))
 app.get('/',(req,res) =>{
     res.send('hi mirachi')
 })
